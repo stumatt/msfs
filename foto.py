9cfba58a9386ae3e73cfcc4ce16721158a99e810
ttteeo <teo.1997@hotmail.it>
2019-12-26 14:10:43 +0100

    Delete foto.py

--- foto.py
+++
@@ -1,6 +0,0 @@
-from fuse import FUSE, FuseOSError, Operations
-
-class MixSliceFS(Operations):
-    def __init__(self, root, mountpoint, masterpassword):
-
-FUSE(MixSliceFS(root,mountpoint,masterpassword), mountpoint, nothreads=True, foreground=True) 
